2dacee32eb4a23997d49869acbf510938d0df7a2
Sergio Marriaga <sergio.marriaga@PERCEPTIO.LOCAL>
2017-05-31 06:20:11 -0500

    inicio travis again2

--- customer/target/generated-sources/annotations/com/udea/customer/entity/DiscountCode_.java
+++ customer/target/generated-sources/annotations/com/udea/customer/entity/DiscountCode_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-31T05:44:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-31T06:19:54")
 @StaticMetamodel(DiscountCode.class)
 public class DiscountCode_ { 
 
